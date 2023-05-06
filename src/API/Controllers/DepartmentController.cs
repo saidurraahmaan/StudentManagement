4596994a429a8b3ad5a22fc6c83ca479a476d9c4
saidurraahmaan <saidurraahmaan@gmail.com>
2023-05-07 01:38:59 +0600

    Add project files.

---
+++ src/API/Controllers/DepartmentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DepartmentController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("get all student");
+        }
+        [HttpGet("{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok("get this " + code + " department data");
+        }
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Ok("Insert new department");
+        }
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok("Insert new department");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("Insert new department");
+        }
+    }
+}
