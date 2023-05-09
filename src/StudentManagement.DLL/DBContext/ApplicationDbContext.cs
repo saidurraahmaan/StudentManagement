df649fc35efbcb691703106e0e2749c648b9e728
saidurraahmaan <saidurraahmaan@gmail.com>
2023-05-09 09:50:31 +0600

    db connection setup

---
+++ src/StudentManagement.DLL/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.DLL.DBContext
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) 
+        {
+            
+        }
+    }
+}
